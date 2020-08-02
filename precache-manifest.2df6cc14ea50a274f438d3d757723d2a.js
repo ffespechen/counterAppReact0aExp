6f8b68d934da9b9d758fcb458a792139329da911
ffespechen <flavio.espechenieva@gmail.com>
2020-08-02 06:39:25 -0300

    Updates

---
+++ precache-manifest.2df6cc14ea50a274f438d3d757723d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914f6b8abd06290f3afadbd7245a4197",
+    "url": "/counterAppReact0aExp/index.html"
+  },
+  {
+    "revision": "2f93db3bb515aea01423",
+    "url": "/counterAppReact0aExp/static/css/main.a6601f16.chunk.css"
+  },
+  {
+    "revision": "9394425a6b8574bfd50e",
+    "url": "/counterAppReact0aExp/static/js/2.87faeb51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counterAppReact0aExp/static/js/2.87faeb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f93db3bb515aea01423",
+    "url": "/counterAppReact0aExp/static/js/main.b39900e8.chunk.js"
+  },
+  {
+    "revision": "65a39b85f7d1e6dec76c",
+    "url": "/counterAppReact0aExp/static/js/runtime-main.b00e6b9a.js"
+  }
+]);
